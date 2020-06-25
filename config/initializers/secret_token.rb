919ac91e1c1b1764b605588d73918ffff269eedd
Mario Gintili <mario.gintili@deliveroo.co.uk>
2020-06-25 09:37:45 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ExampleAppDdReadWriteServices::Application.config.secret_token = 'aeb7b8cdc748fab148d0f6dec48efd8ef73fdac5b1c9f592608de34e3125201e6c3ccd525522c096cd31d020f61aaa981e111cf7bc19253b5a8f51514c92ffee'
